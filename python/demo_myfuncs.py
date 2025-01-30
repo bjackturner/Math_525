1a256e86c29fbd15516cc18c3d2726c5bf580870
Ben Turner <123113101+bjackturner@users.noreply.github.com>
2025-01-30 12:11:14 -0600

    HPC Assignment 3  Completed

--- python/demo_myfuncs.py
+++ python/demo_myfuncs.py
@@ -1,50 +1,85 @@
+import math
 
-def sqrt(x:float, guess:float, max_iter=1000, x_tol=1e-8) -> float:
+# Square root function using Newton method
+def sqrt(x:float, guess:float, max_iter:int=1000, x_tol:float=1e-8) -> float:
     
+    # Check if x is greater than zero
+    if x < 0:
+        raise ValueError(f"x must be greater than or equal to zero")
+
+    # Loop til newton method has converged 
     for i in range(max_iter):
+
+        # Check if Newton method has converged
         if abs(guess**2 - x) <= x_tol: 
             #print(f"Converge after {i} iterations")           
             return guess
         
+        # Perform Newton method
         guess -= (guess**2 - x) / (2*guess)
 
     print(f"Unable to converge after {i} iterations")
     return guess
 
-def factorial(x:int) -> int:
+# Recursive factorial function 
+def factorial(x:int, s:int=1) -> int:
 
-    s = 1
-    for k in range(1,x):
-        s *= k+1
+    # Check if x is greater than zero
+    if x < 0:
+        raise ValueError(f"x must be greater than zero")
+    
+    # Check if x is an integer
+    if type(x) is not int:
+        raise ValueError(f"x must be an integer")
 
-    return s
+    # Recursive factorial 
+    if x == 1:
+        return s
+    elif x == 0:
+        return 1
+    else:
+        return factorial(x - 1, s*x)
 
-def exponential(x:float, n=10) -> float:
+# Determines the value of e^x using a taylor series about the nearest integer 
+def exponential(x:float, num:int=10) -> float:
 
+    # Find the nearest integer and raise e to power of that int
     x0 = int(round(x))
     e_x0 = 2.7182818284590451**x0
     e_x = 0
 
-    for i in range(n):
+    # Perform taylor expansion using factorial function
+    for i in range(num):
         e_x += e_x0 * (x - x0)**i / factorial(i)
 
     return e_x
 
+# Log base e function using Newton method
 def log(x:float, guess:float, max_iter=1000, x_tol=1e-8) -> float:
 
+    # Check if x is greater than zero
+    if x <= 0:
+        raise ValueError(f"x must be greater than zero")
+
+    # Loop until Newton method converges
     for i in range(max_iter):
+
+        # If Newton method converges, return the result
         if abs(exponential(guess) - x) <= x_tol: 
             #print(f"Converge after {i} iterations")           
             return guess
         
+        # Perform Newton method
         guess += -1 + x * exponential(-guess)
 
     print(f"Unable to converge after {i} iterations")
     return guess
 
-n = 4
+num = 10.25
 
-print(f"Sqrt({n}) = {sqrt(n, 0.5)}")
-print(f"factorial({int(n)}) = {factorial(int(n))}")
-print(f"exponential({n}) = {exponential(n)}")
-print(f"log({n}) = {log(n, 0.5)}")
+Completed
+# Outputs
+print(f"Sqrt({num}): \tPython = {math.sqrt(num)} \tMy Function = {sqrt(num, 0.5)} \t Relative difference = {abs(math.sqrt(num) - sqrt(num, 0.5))/math.sqrt(num)}")
+print(f"factorial({int(num)}): \tPython = {math.factorial(int(num))} \t\tMy Function = {factorial(int(num))} \t\t\t Relative difference = {abs(math.factorial(int(num)) - factorial(int(num)))/math.factorial(int(num))}")
+print(f"exp({num}): \tPython = {math.exp(num)} \tMy Function = {exponential(num)} \t Relative difference = {abs(math.exp(num) - exponential(num))/math.exp(num)}")
+print(f"log({num}): \tPython = {math.log(num)} \tMy Function = {log(num, 0.5)} \t Relative difference = {abs(math.log(num) - log(num, 0.5))/math.log(num)}")
